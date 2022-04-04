ead4b5a3ccc1b849d8c63831ed5d8944550788af
Stéphane Bontemps <stephane@bontemps.info>
2022-04-04 22:04:50 +0200

    :feat!(refactoring): use runner as entry point

--- runner.py
+++ runner.py
@@ -1,10 +1,56 @@
 from csvImport import importCSV
+from browse import RSM
+import os
+#import csv as csvReader
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+# importCSV
 # params:
 #  dataSource, csv file
-#  start: first page to read
-#  end: last page to read
-#  offset: first reading is offset + start
+#  start: first entry to browse 
+#  end: last entry to browse
+#  offset: first page browsed is retrieved at entry number (offset + start)
+big_data=[]
+offset = 99900
+start = 0
+end = 5
 
+# import csv data
+listToImport = importCSV("to_use_text.csv",  start, end, offset)
 
-importCSV("to_use_text.csv",  0, 5, 89000)
+count = 0
+nothing = 0
+
+cls()
+
+for url in listToImport:
+    
+    #extract data from url 
+    result = RSM(url[0])
+
+    #append data
+    big_data.append(result)
+
+    #log
+    print("Extract of: " + url[0]) 
+    print("\n")
+    count+=1
+    print("Extract num: "+ str(count) + "/" + str(end-start))
+    if len(result) == 0:
+        print("\n")
+        print("Nothing Extracted, please check format or/and content of the page")
+        nothing+=1
+        print("\n---------------------------------------------------------")
+        
+    print("TOTAL PAGE(s) EXTRACTED :" + str(count - nothing))
+    print("\n TOTAL PAGE(s) WITHOUT RESULT " + str(nothing))
+
+    successRatio = 100-(nothing*(100/count))
+
+    print("\n Success :" + str(int(successRatio)) +"%")
+    print("\n---------------------------------------------------------")
+
+    #save big_data to file
+    #print(big_data)
