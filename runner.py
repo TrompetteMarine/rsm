b4350323b9ad7f83f5696b553d26924a98057700
Stéphane Bontemps <stephane@bontemps.info>
2022-04-04 21:40:46 +0200

    proj(structure):add entry point in runner file

---
+++ runner.py
@@ -0,0 +1,10 @@
+from csvImport import importCSV
+
+# params:
+#  dataSource, csv file
+#  start: first page to read
+#  end: last page to read
+#  offset: first reading is offset + start
+
+
+importCSV("to_use_text.csv",  0, 5, 89000)
